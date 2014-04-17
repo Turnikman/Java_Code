f6e71ee32f73303b675796b4ac5870fec80b1a7d
Aleksey <turnikman.als@gmail.com>
2014-04-17 16:28:57 +0300

    Third commit.

---
+++ Java_Code/src/java_code/Java_Code.java
@@ -0,0 +1,13 @@
+package java_code;
+
+public class Java_Code {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+        c = a + b;
+        System.out.println("Summ= " + c);
+    }
+
+}
